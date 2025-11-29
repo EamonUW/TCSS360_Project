11a19e87848f494791a485a5f003edfac0d21439
merramigora <132627180+merramigora@users.noreply.github.com>
2025-11-29 07:54:46 -0800

    Create AppLauncher.java

---
+++ src/main/java/view/AppLauncher.java
@@ -0,0 +1,10 @@
+package view;
+
+import javax.swing.SwingUtilities;
+
+public class AppLauncher {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new WatcherMainUI());
+    }
+}
